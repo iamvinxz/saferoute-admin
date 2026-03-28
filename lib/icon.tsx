bde424d3cace232180ef2a123713a152e5944565
John Khervin <john.khervin13@gmail.com>
2026-03-28 14:10:07 +0800

    feat(ui): custom icon for markers

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,15 +1,39 @@
+import { create } from "domain";
 import L from "leaflet";
-import { renderToStaticMarkup } from "react-dom/server";
-import { Icon } from "@iconify/react";
 
-export const barangayHall = L.divIcon({
-  html: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color="#042ccb" viewBox="0 0 14 14"><path fill="currentColor" fill-rule="evenodd" d="M7 .05a.75.75 0 0 0-.75.75v2.305L2.963 5.27a.5.5 0 0 0-.204.561l.001.002h8.644v-.002a.5.5 0 0 0-.203-.561L7.75 2.997V1.551h.621a.75.75 0 0 0 0-1.5zm6.242 8.51h-.196v3.934h.196a.75.75 0 0 1 0 1.5H.922a.75.75 0 0 1 0-1.5h.196V8.559H.922a.75.75 0 1 1 0-1.5h12.32a.75.75 0 0 1 0 1.5m-2.106 0h-1.25v3.934h1.25zm-7.022 0h-1.25v3.934h1.25zm3.843 3.934v-2.11a.93.93 0 1 0-1.862 0v2.11z" clip-rule="evenodd"/></svg>`,
-  className: "",
-  iconAnchor: [12, 12],
-});
+const createIcon = (svgContent: string) =>
+  L.divIcon({
+    html: `
+      <div style="
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 30px;
+        height: 30px;
+        background: white;
+        border-radius: 50% 50% 50% 0;
+        transform: rotate(-45deg);
+        box-shadow: 0 2px 6px rgba(0,0,0,0.3);
+      ">
+        <div style="transform: rotate(45deg);">
+          ${svgContent}
+        </div>
+      </div>
+    `,
+    className: "",
+    iconAnchor: [20, 40], // aim the tip of the marker below to the coordinate
+    popupAnchor: [-6, -40], // popup appears above the marker
+  });
 
-export const healthCenter = L.divIcon({
-  html: `<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" color="#cb2b04" viewBox="0 0 24 24"><g fill="currentColor"><path fill-rule="evenodd" d="M4.286 8L12 2l7.714 6H21v3H3V8zm6.964 1V7.75H10v-1.5h1.25V5h1.5v1.25H14v1.5h-1.25V9z" clip-rule="evenodd"/><path d="M21 17v3H3v-3h2v-4.5h2V17h2v-4.5h2V17h2v-4.5h2V17h2v-4.5h2V17z"/></g></svg>`,
-  className: "",
-  iconAnchor: [12, 12],
-});
+export const barangayHall = createIcon(`
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+    <g fill="blue">
+      <path fill-rule="evenodd" d="M4.286 8L12 2l7.714 6H21v3H3V8zm6.964 1V7.75H10v-1.5h1.25V5h1.5v1.25H14v1.5h-1.25V9z" clip-rule="evenodd"/>
+      <path d="M21 17v3H3v-3h2v-4.5h2V17h2v-4.5h2V17h2v-4.5h2V17h2v-4.5h2V17z"/>
+    </g>
+  </svg>
+`);
+
+export const healthCenter = createIcon(
+  `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="#fb542b" viewBox="0 0 24 24"><path fill="currentColor" d="M4 22h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-3V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2m12-7h-3v3h-2v-3H8v-2h3v-3h2v3h3zM9 4h6v2H9z"/></svg>`,
+);
