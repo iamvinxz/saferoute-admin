c428daa6b4f332b396bb57c0c2c9b3603be12856
John Khervin <john.khervin13@gmail.com>
2026-03-29 21:53:35 +0800

    feat:street list of barangay

---
+++ lib/streets.ts
@@ -0,0 +1,140 @@
+export const streets = [
+  {
+    name: "Goldendale Ave",
+  },
+  {
+    name: "Saint James",
+  },
+  {
+    name: "Sisa Ext",
+  },
+  {
+    name: "Platinum",
+  },
+  {
+    name: "Plutonium",
+  },
+  {
+    name: "Arasity",
+  },
+  {
+    name: "Doña Victoria",
+  },
+  {
+    name: "Magnesium",
+  },
+  {
+    name: "Iron",
+  },
+  {
+    name: "Cobalt",
+  },
+  {
+    name: "Titanium",
+  },
+  {
+    name: "Zinc",
+  },
+  {
+    name: "Tungsten",
+  },
+  {
+    name: "Silver",
+  },
+  {
+    name: "Copper",
+  },
+  {
+    name: "Mercury",
+  },
+  {
+    name: "Nickel",
+  },
+  {
+    name: "Celia",
+  },
+  {
+    name: "Governor Wenceslao Pascual Avenue",
+  },
+  {
+    name: "B. Rivera",
+  },
+  {
+    name: "Crispin",
+  },
+  {
+    name: "D. Dela Cruz",
+  },
+  {
+    name: "A. Dizon",
+  },
+  {
+    name: "Sarmiento",
+  },
+  {
+    name: "Sanchez",
+  },
+  {
+    name: "Colonel Espiritu",
+  },
+  {
+    name: "Colonel Espiritu Ext",
+  },
+  {
+    name: "Kab Martin",
+  },
+  {
+    name: "Mesina",
+  },
+  {
+    name: "Sevilla",
+  },
+  {
+    name: "Talilong",
+  },
+  {
+    name: "Kaybasco",
+  },
+  {
+    name: "Villarba",
+  },
+  {
+    name: "Bustamante",
+  },
+  {
+    name: "M.H Del Pilar",
+  },
+  {
+    name: "Sanciangco",
+  },
+  {
+    name: "Mañalac",
+  },
+  {
+    name: "Sociego",
+  },
+  {
+    name: "Trabajo",
+  },
+  {
+    name: "Ezperanza",
+  },
+  {
+    name: "Actividad",
+  },
+  {
+    name: "Concha",
+  },
+  {
+    name: "Mejorada",
+  },
+  {
+    name: "Perla",
+  },
+  {
+    name: "Kaingin Road",
+  },
+  {
+    name: "",
+  },
+];
