d5f8db4dc7748345eb64efa69d086d7150605a6b
John Khervin <john.khervin13@gmail.com>
2026-04-15 14:16:27 +0800

    feat:toast provider

--- components/LayoutProvider.tsx
+++ components/LayoutProvider.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { store } from "@/state/store";
+import { Toaster } from "@/components/ui/sonner";
 import { Provider } from "react-redux";
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {children}
+      <Toaster />
+    </Provider>
+  );
 };
 
 export default LayoutProvider;
