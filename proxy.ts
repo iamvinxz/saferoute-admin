a20aaa75fb37ffc2eddf8d6ab369cfd654cf66bc
John Khervin <john.khervin13@gmail.com>
2026-04-15 14:06:37 +0800

    feat:middleware

---
+++ proxy.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const PROTECTED_ROUTES = ["/dashboard", "/maps", "/notification"];
+const AUTH_ROUTES = ["/auth/login"];
+
+export function proxy(req: NextRequest) {
+  //retrieve the token in the cookies
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  const isProtectedRoute = PROTECTED_ROUTES.some((route) =>
+    pathname.startsWith(route),
+  );
+  const isAuthRoute = AUTH_ROUTES.some((route) => pathname.startsWith(route));
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+
+  // Already logged in → trying to access login page (back button case)
+  if (isAuthRoute && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  const response = NextResponse.next();
+  if (isProtectedRoute) {
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate",
+    );
+    response.headers.set("Pragma", "no-cache");
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
