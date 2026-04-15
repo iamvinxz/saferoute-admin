2f091f1cef7b7ab36e18ffff6c18c49ec1aeaec6
John Khervin <john.khervin13@gmail.com>
2026-04-15 14:07:57 +0800

    feat:rtk query for geojson fetch

---
+++ Redux/Services/mapService.ts
@@ -0,0 +1,36 @@
+import { api } from "./APIService";
+
+export const mapsApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getGeoJson: builder.query<any, void>({
+      queryFn: async () => {
+        try {
+          const url = process.env.NEXT_PUBLIC_GEOJSON_URI;
+          if (!url)
+            return {
+              error: {
+                status: "FETCH_ERROR",
+                error: "GeoJSON URL is not defined!",
+              },
+            };
+
+          const response = await fetch(url);
+          const data = await response.json();
+
+          const tinajeros = {
+            ...data,
+            features: data.features.filter((f: any) =>
+              f.properties.adm4_en?.toLowerCase().includes("tinajeros"),
+            ),
+          };
+
+          return { data: tinajeros };
+        } catch (error) {
+          return { error: { status: "FETCH_ERROR", error: String(error) } };
+        }
+      },
+    }),
+  }),
+});
+
+export const { useGetGeoJsonQuery } = mapsApi;
