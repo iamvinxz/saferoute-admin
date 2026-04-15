b10f04b9202e06734d0230feef5624fc06b3f752
John Khervin <john.khervin13@gmail.com>
2026-04-15 14:15:57 +0800

    feat:login form

--- components/FormsField.tsx
+++ components/FormsField.tsx
@@ -1,19 +1,44 @@
 "use client";
-import { useForm } from "react-hook-form";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldSet,
-  FieldTitle,
-} from "./ui/field";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Field, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useLoginMutation } from "@/Redux/Services/authService";
+import { useDispatch } from "react-redux";
+import { setUser } from "@/state/slices/authSlice";
+import type { AppDispatch } from "@/state/store";
+
+interface FormsFields {
+  email: string;
+  password: string;
+}
 
 const FormsField = () => {
-  const { register } = useForm();
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+  const { register, handleSubmit } = useForm<FormsFields>();
+
+  //rtk query
+  const [signIn] = useLoginMutation();
+
+  //handle functions
+  const handleSignIn: SubmitHandler<FormsFields> = async (data, e) => {
+    e?.preventDefault();
+
+    try {
+      const credentials = await signIn({
+        email: data.email,
+        password: data.password,
+      }).unwrap();
+
+      dispatch(setUser(credentials.user));
+
+      router.push("/dashboard");
+    } catch (error) {}
+  };
+
   return (
     <div className="p-10">
       <div className="flex flex-col items-center justify-center mt-5">
@@ -25,24 +50,31 @@ const FormsField = () => {
           height={200}
         />
       </div>
-
-      <div className="space-y-5 mb-20">
-        <FieldGroup>
-          <Field>
-            <FieldLabel className="text-md">Email</FieldLabel>
-            <Input className="h-10" />
-          </Field>
-        </FieldGroup>
-        <FieldGroup>
-          <Field>
-            <FieldLabel className="text-md">Password</FieldLabel>
-            <Input className="h-10" />
-          </Field>
-        </FieldGroup>
-      </div>
-      <div className="my-5">
-        <Button className="w-full h-12 bg-[#5f7ed1]">Submit</Button>
-      </div>
+      <form onSubmit={handleSubmit(handleSignIn)}>
+        <div className="space-y-5 mb-20">
+          <FieldGroup>
+            <Field>
+              <FieldLabel className="text-md">Email</FieldLabel>
+              <Input type="email" {...register("email")} className="h-10" />
+            </Field>
+          </FieldGroup>
+          <FieldGroup>
+            <Field>
+              <FieldLabel className="text-md">Password</FieldLabel>
+              <Input
+                type="password"
+                {...register("password")}
+                className="h-10"
+              />
+            </Field>
+          </FieldGroup>
+        </div>
+        <div className="my-5">
+          <Button className="w-full h-12 bg-[#5f7ed1]" type="submit">
+            Submit
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
