06bf4316ca027e5a08ee70595f96c610877e5681
John Khervin <john.khervin13@gmail.com>
2026-04-17 00:09:12 +0800

    feat:redux state of segment

---
+++ state/slices/segment.ts
@@ -0,0 +1,69 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export type FloodReport = {
+  imageUrl: string;
+  streetName: string;
+  depth: string;
+};
+
+type Segment = {
+  points: [number, number][];
+  coords: [number, number][];
+  floodReport: FloodReport;
+};
+
+type SegmentState = {
+  segments: Segment[];
+};
+
+const initialSegment: Segment = {
+  points: [],
+  coords: [],
+  floodReport: { imageUrl: "", streetName: "", depth: "" },
+};
+
+const initialState: SegmentState = {
+  segments: [initialSegment],
+};
+
+const segmentSlice = createSlice({
+  name: "segment",
+  initialState,
+  reducers: {
+    addPoint(state, action: PayloadAction<[number, number]>) {
+      const last = state.segments[state.segments.length - 1];
+      last.points.push(action.payload);
+    },
+
+    updateCoords(state, action: PayloadAction<[number, number][]>) {
+      const last = state.segments[state.segments.length - 1];
+      last.coords = action.payload;
+    },
+
+    addSegment(state) {
+      state.segments.push({
+        ...initialSegment,
+        points: [],
+        coords: [],
+        floodReport: { imageUrl: "", streetName: "", depth: "" },
+      });
+    },
+
+    updateFloorReport(
+      state,
+      action: PayloadAction<{
+        index: number;
+        field: keyof FloodReport;
+        value: string;
+      }>,
+    ) {
+      const { index, field, value } = action.payload;
+      state.segments[index].floodReport[field] = value;
+    },
+  },
+});
+
+export const { addPoint, updateCoords, updateFloorReport, addSegment } =
+  segmentSlice.actions;
+
+export default segmentSlice.reducer;
--- state/store.ts
+++ state/store.ts
@@ -1,11 +1,13 @@
 import { api } from "@/Redux/Services/APIService";
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/state/slices/authSlice";
+import segmentReducer from "@/state/slices/segment";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
+    segment: segmentReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
