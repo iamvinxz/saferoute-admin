79659cb3274e28c06798afe9dff21bef3664ad4b
John Khervin <john.khervin13@gmail.com>
2026-04-21 22:21:03 +0800

    fix: crosshair cursor fixed inside the geojson

--- components/map/CursorController.tsx
+++ components/map/CursorController.tsx
@@ -1,14 +1,47 @@
 import { useEffect } from "react";
-import { useMap } from "react-leaflet";
+import { useMap, useMapEvents } from "react-leaflet";
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
+import { point } from "@turf/helpers";
 
-const CursorController = ({ isRoutingMode }: { isRoutingMode: boolean }) => {
+type Props = {
+  isRoutingMode: boolean;
+  geoJsonData: GeoJSON.FeatureCollection;
+};
+
+const CursorController = ({ isRoutingMode, geoJsonData }: Props) => {
   const map = useMap();
 
   useEffect(() => {
     const container = map.getContainer();
-    container.style.cursor = isRoutingMode ? "crosshair" : "";
-  }, [isRoutingMode, map]);
 
+    const updateCursor = (e: MouseEvent) => {
+      if (!isRoutingMode) {
+        container.classList.remove("cursor-crosshair");
+        return;
+      }
+
+      //converts mouse pixel to lat lng
+      const latlng = map.mouseEventToLatLng(e);
+
+      //converts coordinate to turf helper
+      const mousePosition = point([latlng.lng, latlng.lat]);
+      const isInside = geoJsonData?.features.some((feature) =>
+        booleanPointInPolygon(
+          mousePosition,
+          feature as GeoJSON.Feature<GeoJSON.Polygon>,
+        ),
+      );
+
+      if (isInside) {
+        container.classList.add("cursor-crosshair");
+      } else {
+        container.classList.remove("cursor-crosshair");
+      }
+    };
+
+    container.addEventListener("mousemove", updateCursor);
+    return () => container.removeEventListener("mousemove", updateCursor);
+  }, [isRoutingMode, map, geoJsonData]);
   return null;
 };
 
