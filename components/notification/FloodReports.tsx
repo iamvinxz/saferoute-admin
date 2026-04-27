893f64b812fe90fbcbe688925d9f610d67a519c6
John Khervin <john.khervin13@gmail.com>
2026-04-27 09:38:01 +0800

    fix: adjust ui

--- components/notification/FloodReports.tsx
+++ components/notification/FloodReports.tsx
@@ -3,9 +3,10 @@ import FloodReportCard from "./FloodReportCard";
 const FloodReports = () => {
   const reports = [
     {
-      streetName: "J.P Rizal Ave",
+      streetName: "Colonel Espiritu Street",
       depth: "Knee-Deep",
       description: "Flood here is very severe, road is impassable.",
+      coordinates: [14.673379504810953, 120.96788406372072] as [number, number],
       imageUrl: "/japan.png",
       reportedAt: "Today, 10:32 AM",
     },
@@ -13,13 +14,15 @@ const FloodReports = () => {
       streetName: "Crispin Street",
       depth: "Ankle-Deep",
       description: "Minor flooding near the intersection.",
+      coordinates: [14.670584962500524, 120.96884161233903] as [number, number],
       imageUrl: "/street.jpg",
       reportedAt: "Today, 9:15 AM",
     },
     {
-      streetName: "Blumentritt Road",
+      streetName: "Mañalac Street",
       depth: "Critical",
       description: "Road completely submerged, avoid this route.",
+      coordinates: [14.671251815671544, 120.96389293670656] as [number, number],
       imageUrl: "/night-street.jpg",
       reportedAt: "Today, 8:50 AM",
     },
