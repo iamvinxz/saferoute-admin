daf15048c09402a31d51f950e5630229671f069e
John Khervin <john.khervin13@gmail.com>
2026-04-27 17:34:28 +0800

    feat: rtk query for create pin and handle submit function

--- components/map/FloodIncidentSheet.tsx
+++ components/map/FloodIncidentSheet.tsx
@@ -8,6 +8,8 @@ import { removeSegment, updateFloodReport } from "@/state/slices/segment";
 import { setDescription, setPinName, removePin } from "@/state/slices/pinSlice";
 import { FloodReport } from "@/state/slices/segment";
 import { x } from "@/lib/icon";
+import { useCreatePinMutation } from "@/Redux/Services/markService";
+import { toast } from "sonner";
 
 const FloodReportSheet = () => {
   const dispatch = useDispatch();
@@ -17,6 +19,9 @@ const FloodReportSheet = () => {
   const pins = useSelector((state: RootState) => state.pin.pins);
   const [visible, setVisible] = useState(false);
   const divRef = useRef<HTMLDivElement>(null);
+
+  //rtk query
+  const [createPin, { isLoading }] = useCreatePinMutation();
 
   useEffect(() => {
     if (isRoutingMode || isPinMode) {
@@ -39,6 +44,23 @@ const FloodReportSheet = () => {
   };
 
   if (!visible) return null;
+
+  const handleSubmit = async () => {
+    try {
+      for (const pin of pins) {
+        await createPin({
+          latitude: pin.coords[0],
+          longitude: pin.coords[1],
+          pinName: pin.pinName,
+          description: pin.description,
+        }).unwrap();
+      }
+
+      toast.success("Pinned location created!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div
@@ -148,8 +170,12 @@ const FloodReportSheet = () => {
         )}
       </div>
 
-      <Button className="mt-auto w-full rounded-lg bg-gray-900 py-2.5 text-sm font-medium text-white hover:bg-gray-700">
-        Submit report
+      <Button
+        onClick={handleSubmit}
+        disabled={isLoading}
+        className="mt-auto w-full rounded-lg bg-gray-900 py-2.5 text-sm font-medium text-white hover:bg-gray-700"
+      >
+        {isLoading ? "Submitting" : "Submit report"}
       </Button>
     </div>
   );
