2c4973a979ee7d7de73e8e77c4212f6d99b66d4c
John Khervin <john.khervin13@gmail.com>
2026-04-28 10:49:25 +0800

    fix: env variable

--- Redux/Services/APIService.ts
+++ Redux/Services/APIService.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   credentials: "include",
   prepareHeaders: (headers) => {
     headers.set("Content-Type", "application/json");
