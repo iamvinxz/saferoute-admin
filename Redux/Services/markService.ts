94c69b3e4df44cbfdf063354279fabe78e0d5512
John Khervin <john.khervin13@gmail.com>
2026-04-28 15:58:32 +0800

    feat: create segment endpoint

--- Redux/Services/markService.ts
+++ Redux/Services/markService.ts
@@ -1,5 +1,9 @@
 import { api } from "./APIService";
-import { CREATE_PIN, GET_ALL_PINNED_LOCATIONS } from "./Endpoints";
+import {
+  CREATE_PIN,
+  CREATE_SEGMENT,
+  GET_ALL_PINNED_LOCATIONS,
+} from "./Endpoints";
 
 export const markApi = api.injectEndpoints({
   endpoints: (build) => ({
@@ -13,12 +17,22 @@ export const markApi = api.injectEndpoints({
       MarkControllerPinResponse,
       MarkControllerPinRequest
     >({
-      query: (payload) => ({
+      query: (pin) => ({
         url: CREATE_PIN,
         method: "POST",
-        body: payload,
+        body: pin,
       }),
       invalidatesTags: ["Pins"],
+    }),
+    createSegment: build.mutation<
+      MarkControllerSegmentResponse,
+      MarkControllerSegmentRequest
+    >({
+      query: (segments) => ({
+        url: CREATE_SEGMENT,
+        method: "POST",
+        body: segments,
+      }),
     }),
   }),
   overrideExisting: true,
@@ -45,4 +59,38 @@ type MarkControllerPinResponse = {
   ];
 };
 
-export const { useGetAllPinQuery, useCreatePinMutation } = markApi;
+type MarkControllerSegmentRequest = {
+  points: [number, number][];
+  coords: [number, number][];
+  description: string;
+  floodDepth: string;
+  streetName: string;
+};
+
+type FloodReport = {
+  reportedBy: string;
+  description: string;
+  floodDepth: string;
+  streeName: string;
+  _id: string; // id of floodreport
+  createdAt: string;
+  updatedAt: string;
+};
+
+type MarkControllerSegmentResponse = {
+  message: string;
+  segment: {
+    points: [number, number][];
+    coords: [number, number][];
+    floodReport: FloodReport;
+  };
+  _id: string; // id of segment
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const {
+  useGetAllPinQuery,
+  useCreatePinMutation,
+  useCreateSegmentMutation,
+} = markApi;
