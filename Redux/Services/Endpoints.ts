2364174662c679be6cbecd7ecae2724e7791c34b
John Khervin <john.khervin13@gmail.com>
2026-04-28 10:15:29 +0800

    feat: endpoint for get all pinned location

--- Redux/Services/Endpoints.ts
+++ Redux/Services/Endpoints.ts
@@ -1,3 +1,4 @@
 export const LOGIN = "/api/auth/login";
 export const LOGOUT = "/api/auth/logout";
+export const GET_ALL_PINNED_LOCATIONS = "/api/mark/get/all";
 export const CREATE_PIN = "/api/mark/createPin";
