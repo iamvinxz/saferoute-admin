569deb8f1aae7476c6192434f82c9a40c3584c2d
John Khervin <john.khervin13@gmail.com>
2026-04-28 15:57:53 +0800

    feat: create segment endpoint path

--- Redux/Services/Endpoints.ts
+++ Redux/Services/Endpoints.ts
@@ -2,3 +2,4 @@ export const LOGIN = "/api/auth/login";
 export const LOGOUT = "/api/auth/logout";
 export const GET_ALL_PINNED_LOCATIONS = "/api/mark/pin/all";
 export const CREATE_PIN = "/api/mark/createPin";
+export const CREATE_SEGMENT = "/api/mark/createSegment";
