60670f8dc01375ed8011a6bff7c1051d793124b9
John Khervin <john.khervin13@gmail.com>
2026-04-28 10:27:38 +0800

    feat: no content display

---
+++ components/notification/noContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoContent = () => {
+  return (
+    <div className="flex items-center justify-center h-full border border-[#e6e6e6] rounded-md">
+      <span className="text-[#9e9c9c] text-sm">No selected report.</span>
+    </div>
+  );
+};
+
+export default NoContent;
