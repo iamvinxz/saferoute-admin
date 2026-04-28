ef4f8a0b310c5553a609cdd2a3d145bafaad455b
John Khervin <john.khervin13@gmail.com>
2026-04-28 16:12:33 +0800

    feat: get all segment location endpoint path

--- Redux/Services/Endpoints.ts
+++ Redux/Services/Endpoints.ts
@@ -3,3 +3,4 @@ export const LOGOUT = "/api/auth/logout";
 export const GET_ALL_PINNED_LOCATIONS = "/api/mark/pin/all";
 export const CREATE_PIN = "/api/mark/createPin";
 export const CREATE_SEGMENT = "/api/mark/createSegment";
+export const GET_ALL_SEGMENT_LOCATIONS = "/api/mark/segment/all";
