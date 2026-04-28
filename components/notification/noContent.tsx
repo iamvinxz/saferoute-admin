a783bdbec85265210e67dd2165563537ba82c662
John Khervin <john.khervin13@gmail.com>
2026-04-28 13:34:33 +0800

    fix: delete component

--- components/notification/noContent.tsx
+++
@@ -1,9 +0,0 @@
-const NoContent = () => {
-  return (
-    <div className="flex items-center justify-center h-full border border-[#e6e6e6] rounded-md">
-      <span className="text-[#9e9c9c] text-sm">No selected report.</span>
-    </div>
-  );
-};
-
-export default NoContent;
