6c34ce7c32c196356fdb2627ca740a45c045bf94
John Khervin <john.khervin13@gmail.com>
2026-04-29 12:38:22 +0800

    feat: controller tab for view flooded street lists

--- components/map/ControllerTab.tsx
+++ components/map/ControllerTab.tsx
@@ -1,4 +1,4 @@
-import { shelterIcon, barangay, center } from "@/lib/icon";
+import { shelterIcon, barangay, center, floodedStreet } from "@/lib/icon";
 
 interface FocusTarget {
   lat: number;
@@ -37,17 +37,23 @@ const ControllerTab = ({ onFocus }: Props) => {
     },
   ];
   return (
-    <div className="flex gap-4 absolute top-3 left-15 z-1000">
-      {controllers.map((ctrl, index) => (
-        <button
-          key={index}
-          onClick={() => onFocus(ctrl.coordinates)} // go to specific marker
-          className="flex items-center gap-2 bg-white px-3 py-2 rounded-lg shadow-md hover:bg-blue-50 hover:shadow-lg transition-all cursor-pointer"
-        >
-          <span dangerouslySetInnerHTML={{ __html: ctrl.icon }} />
-          <span className="text-sm font-medium">{ctrl.name}</span>
-        </button>
-      ))}
+    <div className="absolute top-3 left-15 z-400 flex items-center w-full justify-between">
+      <div className="flex gap-4">
+        {controllers.map((ctrl, index) => (
+          <button
+            key={index}
+            onClick={() => onFocus(ctrl.coordinates)} // go to specific marker
+            className="flex items-center gap-2 bg-white px-3 py-2 rounded-lg shadow-md hover:bg-blue-50 hover:shadow-lg transition-all cursor-pointer"
+          >
+            <span dangerouslySetInnerHTML={{ __html: ctrl.icon }} />
+            <span className="text-sm font-medium">{ctrl.name}</span>
+          </button>
+        ))}
+      </div>
+      <div className="bg-white px-3 py-2 rounded-md shadow-md relative right-20 flex items-center gap-2 hover:bg-blue-50 hover:shadow-lg">
+        <span dangerouslySetInnerHTML={{ __html: floodedStreet }} />
+        View Flooded Streets
+      </div>
     </div>
   );
 };
