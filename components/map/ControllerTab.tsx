87a7a4c6aa5ce5441ccc27ccad85cb454e2c0f16
John Khervin <john.khervin13@gmail.com>
2026-04-29 18:22:10 +0800

    fix: add props for set view flooded street setter toggle

--- components/map/ControllerTab.tsx
+++ components/map/ControllerTab.tsx
@@ -1,4 +1,5 @@
 import { shelterIcon, barangay, center, floodedStreet } from "@/lib/icon";
+import { useState } from "react";
 
 interface FocusTarget {
   lat: number;
@@ -7,54 +8,61 @@ interface FocusTarget {
 
 interface Props {
   onFocus: (target: FocusTarget) => void;
+  onToggle: () => void;
 }
 
-const ControllerTab = ({ onFocus }: Props) => {
-  const controllers = [
-    {
-      icon: shelterIcon,
-      name: "Evacuation Center",
-      coordinates: {
-        lat: 14.671372083129416,
-        lng: 120.97004644282855,
-      },
+const controllers = [
+  {
+    icon: shelterIcon,
+    name: "Evacuation Center",
+    coordinates: {
+      lat: 14.671372083129416,
+      lng: 120.97004644282855,
     },
-    {
-      icon: barangay,
-      name: "Barangay Hall",
-      coordinates: {
-        lat: 14.671713871663536,
-        lng: 120.97042400298534,
-      },
+  },
+  {
+    icon: barangay,
+    name: "Barangay Hall",
+    coordinates: {
+      lat: 14.671713871663536,
+      lng: 120.97042400298534,
     },
-    {
-      icon: center,
-      name: "Health Center",
-      coordinates: {
-        lat: 14.671671704218502,
-        lng: 120.97030390962857,
-      },
+  },
+  {
+    icon: center,
+    name: "Health Center",
+    coordinates: {
+      lat: 14.671671704218502,
+      lng: 120.97030390962857,
     },
-  ];
+  },
+];
+
+const ControllerTab = ({ onFocus, onToggle }: Props) => {
   return (
-    <div className="absolute top-3 left-15 z-400 flex items-center w-full justify-between">
-      <div className="flex gap-4">
-        {controllers.map((ctrl, index) => (
-          <button
-            key={index}
-            onClick={() => onFocus(ctrl.coordinates)} // go to specific marker
-            className="flex items-center gap-2 bg-white px-3 py-2 rounded-lg shadow-md hover:bg-blue-50 hover:shadow-lg transition-all cursor-pointer"
-          >
-            <span dangerouslySetInnerHTML={{ __html: ctrl.icon }} />
-            <span className="text-sm font-medium">{ctrl.name}</span>
-          </button>
-        ))}
+    <>
+      <div className="absolute top-3 left-15 z-400 flex items-center w-436 justify-between">
+        <div className="flex gap-4">
+          {controllers.map((ctrl, index) => (
+            <button
+              key={index}
+              onClick={() => onFocus(ctrl.coordinates)} // go to specific marker
+              className="flex items-center gap-2 bg-white px-3 py-2 rounded-lg shadow-md hover:bg-blue-50 hover:shadow-lg transition-all cursor-pointer"
+            >
+              <span dangerouslySetInnerHTML={{ __html: ctrl.icon }} />
+              <span className="text-sm font-medium">{ctrl.name}</span>
+            </button>
+          ))}
+        </div>
+        <button
+          className="bg-white px-3 py-2 rounded-md shadow-md relative right-5 flex items-center gap-2 hover:bg-blue-50 hover:shadow-lg cursor-pointer w-60"
+          onClick={() => onToggle()}
+        >
+          <span dangerouslySetInnerHTML={{ __html: floodedStreet }} />
+          <span className="text-sm font-medium">View Flooded Streets</span>
+        </button>
       </div>
-      <div className="bg-white px-3 py-2 rounded-md shadow-md relative right-20 flex items-center gap-2 hover:bg-blue-50 hover:shadow-lg">
-        <span dangerouslySetInnerHTML={{ __html: floodedStreet }} />
-        View Flooded Streets
-      </div>
-    </div>
+    </>
   );
 };
 
