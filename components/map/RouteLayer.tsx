e7612667f6450c283619afade6a1f20c38e258fe
John Khervin <john.khervin13@gmail.com>
2026-04-29 10:48:40 +0800

    fix: adjust popup ui design

--- components/map/RouteLayer.tsx
+++ components/map/RouteLayer.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useEffect } from "react";
-import { Polyline, CircleMarker } from "react-leaflet";
+import { Polyline, CircleMarker, Popup } from "react-leaflet";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/state/store";
 import ClickCapture from "@/components/map/ClickCapture";
@@ -8,10 +8,21 @@ import { addPoint, addSegment, updateCoords } from "@/state/slices/segment";
 import { fetchOSRMRoute } from "@/lib/fetchOSRMRoute";
 import { toast } from "sonner";
 import { useGetAllSegmentQuery } from "@/Redux/Services/markService";
+import { depthColors } from "@/components/notification/FloodReportCard";
 
 interface Props {
   geoJsonData: GeoJSON.FeatureCollection;
 }
+
+const getDepthColors = (depth: string) => {
+  const match = Object.keys(depthColors).find(
+    (key) => key.toLowerCase() === depth.toLowerCase(),
+  );
+
+  return (
+    depthColors[match ?? ""] ?? { badge: "bg-gray-100", text: "text-gray-700" }
+  );
+};
 
 const RouteLayer = ({ geoJsonData }: Props) => {
   const segments = useSelector((state: RootState) => state.segment.segments);
@@ -98,7 +109,39 @@ const RouteLayer = ({ geoJsonData }: Props) => {
           key={index}
           positions={segment.coords}
           pathOptions={{ color: "#ff0000", weight: 4, opacity: 0.8 }}
-        />
+        >
+          <Popup>
+            <div className="min-w-50 max-w-70">
+              <div className="flex items-center justify-between">
+                <span
+                  className={`font-bold text-[#303030] leading-tight wrap-break-words ${
+                    segment.floodReport.streetName.length > 10
+                      ? "text-sm"
+                      : segment.floodReport.streetName.length > 8
+                        ? "text-base"
+                        : "text-lg"
+                  }`}
+                >
+                  {segment.floodReport.streetName}
+                </span>
+                <span
+                  className={`text-xs font-medium px-2 py-0.5 rounded-full whitespace-nowrap shrink-0
+                    ${getDepthColors(segment.floodReport.floodDepth).badge}
+                    ${getDepthColors(segment.floodReport.floodDepth).text}
+                  `}
+                >
+                  {segment.floodReport.floodDepth}
+                </span>
+              </div>
+
+              {segment.floodReport.description && (
+                <span className="text-xs text-gray-500 leading-relaxed">
+                  {segment.floodReport.description}
+                </span>
+              )}
+            </div>
+          </Popup>
+        </Polyline>
       ))}
     </>
   );
