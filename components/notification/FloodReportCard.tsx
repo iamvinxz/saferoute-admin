05bae39a318a775f6b27d29d6ac2d8210275d328
John Khervin <john.khervin13@gmail.com>
2026-05-04 09:29:03 +0800

    feat: rtk query fetch for all flood report

--- components/notification/FloodReportCard.tsx
+++ components/notification/FloodReportCard.tsx
@@ -3,116 +3,105 @@ import { MapPin, Droplets, Clock } from "lucide-react";
 import { eye } from "@/lib/icon";
 import { useDispatch } from "react-redux";
 import { setReport } from "@/state/slices/selectedReport";
+import { getDepthColors } from "@/lib/colorHelper";
+import { useGetAllFloodReportQuery } from "@/Redux/Services/floodReportService";
+import { FloodReportCardSkeleton } from "./FloodReportCardSkeleton";
 
-export const depthColors: Record<
-  string,
-  { bg: string; text: string; badge: string }
-> = {
-  "Ankle-Deep": {
-    bg: "bg-yellow-50",
-    text: "text-yellow-700",
-    badge: "bg-yellow-100",
-  },
-  "Knee-Deep": {
-    bg: "bg-orange-50",
-    text: "text-orange-700",
-    badge: "bg-orange-100",
-  },
-  "Chest-Deep": {
-    bg: "bg-red-50",
-    text: "text-red-700",
-    badge: "bg-red-100",
-  },
-  Critical: {
-    bg: "bg-red-100",
-    text: "text-red-800",
-    badge: "bg-red-200",
-  },
-};
-
-type FloodReportCardProps = {
-  index: number;
-  streetName: string;
-  depth: string;
-  description: string;
-  imageUrl?: string;
-  reportedAt?: string;
-  coordinates?: [number, number];
-};
-
-const FloodReportCard = ({
-  index,
-  streetName,
-  depth,
-  description,
-  imageUrl,
-  reportedAt,
-  coordinates,
-}: FloodReportCardProps) => {
+const FloodReportCard = () => {
   const dispatch = useDispatch();
-  const colors = depthColors[depth] ?? depthColors["Knee-Deep"];
+
+  //rtk query
+  const { data: floodReports, isLoading: loadingFloodReports } =
+    useGetAllFloodReportQuery();
+
+  if (loadingFloodReports) return <FloodReportCardSkeleton />;
+
+  if (!floodReports || floodReports.reports.length === 0)
+    return (
+      <div className="h-175 text-[#979797] text-sm flex items-center justify-center">
+        No flood reports yet.
+      </div>
+    );
 
   return (
-    <div className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden hover:shadow-md transition-shadow duration-200">
-      {/* Image */}
-      {imageUrl && (
-        <div className="relative w-full h-40">
-          <Image
-            src={imageUrl}
-            alt={streetName}
-            fill
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent" />
-        </div>
-      )}
-
-      <div className="p-4 space-y-3">
-        {/* Header */}
-        <div className="flex items-start justify-between gap-2">
-          <div className="flex items-center gap-1.5">
-            <MapPin className="w-4 h-4 text-gray-400 shrink-0" />
-            <span className="font-semibold text-gray-800 text-sm">
-              {streetName}
-            </span>
-          </div>
-          <span
-            className={`flex items-center gap-1 text-xs font-medium px-2.5 py-1 rounded-full shrink-0 ${colors.badge} ${colors.text}`}
+    <div className="space-y-5">
+      {floodReports?.reports.map((report, index) => {
+        const colors = getDepthColors(report.floodDepth);
+        return (
+          <div
+            key={report._id}
+            className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden hover:shadow-md transition-shadow duration-200"
           >
-            <Droplets className="w-3 h-3" />
-            {depth}
-          </span>
-        </div>
+            {report.photoUrl && (
+              <div className="relative w-full h-40">
+                <Image
+                  src={report.photoUrl}
+                  alt={report.streetName}
+                  fill
+                  className="object-cover"
+                />
+                <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent" />
+              </div>
+            )}
 
-        {/* Description */}
-        <p className="text-xs text-gray-500 leading-relaxed">{description}</p>
+            <div className="p-4 space-y-3">
+              <div className="flex items-start justify-between gap-2">
+                <div className="flex items-center gap-1.5">
+                  <MapPin className="w-4 h-4 text-gray-400 shrink-0" />
+                  <span className="font-semibold text-gray-800 text-sm">
+                    {report.streetName}
+                  </span>
+                </div>
+                <span
+                  className={`flex items-center gap-1 capitalize text-xs font-medium px-2.5 py-1 rounded-full shrink-0 ${colors.badge} ${colors.text}`}
+                >
+                  <Droplets className="w-3 h-3" />
+                  {report.floodDepth}
+                </span>
+              </div>
 
-        <div className="flex justify-between items-center">
-          {/* Footer */}
-          {reportedAt && (
-            <div className="flex items-center gap-1.5 pt-1 border-t border-gray-50">
-              <Clock className="w-3 h-3 text-gray-300" />
-              <span className="text-[11px] text-gray-400">{reportedAt}</span>
+              <p className="text-xs text-gray-500 leading-relaxed">
+                {report.description}
+              </p>
+
+              <div className="flex justify-between items-center">
+                {report.createdAt && (
+                  <div className="flex items-center gap-1.5 pt-1 border-t border-gray-50">
+                    <Clock className="w-3 h-3 text-gray-300" />
+                    <span className="text-[11px] text-gray-400">
+                      {new Date(report.createdAt).toLocaleString("en-US", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                        hour: "numeric",
+                        minute: "2-digit",
+                        hour12: true,
+                      })}
+                    </span>
+                  </div>
+                )}
+                <span
+                  dangerouslySetInnerHTML={{ __html: eye }}
+                  className="hover:cursor-pointer"
+                  onClick={() =>
+                    dispatch(
+                      setReport({
+                        index,
+                        id: report._id,
+                        streetName: report.streetName,
+                        depth: report.floodDepth,
+                        description: report.description,
+                        imageUrl: report.photoUrl,
+                        coordinates: report.coords,
+                      }),
+                    )
+                  }
+                />
+              </div>
             </div>
-          )}
-          <span
-            dangerouslySetInnerHTML={{ __html: eye }}
-            className="hover:cursor-pointer"
-            onClick={() =>
-              dispatch(
-                setReport({
-                  index,
-                  streetName,
-                  depth,
-                  description,
-                  imageUrl,
-                  coordinates,
-                }),
-              )
-            }
-          />
-        </div>
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
