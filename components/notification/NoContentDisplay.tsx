a9b0325047b6c17f487689b57fd3e0584edff412
John Khervin <john.khervin13@gmail.com>
2026-05-04 09:27:05 +0800

    feat: added css

--- components/notification/NoContentDisplay.tsx
+++ components/notification/NoContentDisplay.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const NoContentDisplay = () => {
-  return <div>NoContentDisplay</div>;
+  return (
+    <div className="w-full h-full border border-[#ebebeb] rounded-md bg-[#f5f5f579] flex items-center justify-center">
+      <div className="text-sm text-[#979797]">No selected report.</div>
+    </div>
+  );
 };
 
 export default NoContentDisplay;
