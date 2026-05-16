57c2c60022d9f0bd6b497bacb5869171c2678e79
iamvinxz <john.khervin13@gmail.com>
2026-05-17 02:43:18 +0800

    fix: removed md responsive design

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen flex">
       <SideBar />
-      <main className="flex-1 max-sm:ml-0 h-screen overflow-auto transition-all duration-300 ml-0 md:ml-(--sidebar-w,7.5rem)">
+      <main className="flex-1 max-sm:ml-0 h-screen overflow-auto transition-all duration-300 ml-0">
         {children}
       </main>
     </div>
