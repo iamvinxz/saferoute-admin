9e6d1942db814d7697a2f23a48f64af4a5e1446d
iamvinxz <john.khervin13@gmail.com>
2026-05-17 02:47:43 +0800

    fix: deleted the file and move the header content to the page.tsx

--- app/(dashboard)/notification/layout.tsx
+++
@@ -1,7 +0,0 @@
-import NotificationLayout from "@/components/notification/NotificationLayout";
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <NotificationLayout>{children}</NotificationLayout>;
-};
-
-export default layout;
