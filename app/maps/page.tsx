b84270e1c17c05823a3f2cfc2dfa7dc81b31c36c
iamvinxz <john.khervin13@gmail.com>
2026-05-18 16:07:04 +0800

    feat: added loading screen

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,10 +1,16 @@
 "use client";
+import LoadingScreen from "@/components/LoadingScreen";
 import dynamic from "next/dynamic";
-
-const Map = dynamic(() => import("@/components/Map"), {
-  ssr: false,
-  loading: () => <p>Loading map...</p>,
-});
+const Map = dynamic(
+  () =>
+    new Promise((resolve) => setTimeout(resolve, 2000)).then(
+      () => import("@/components/Map"),
+    ),
+  {
+    ssr: false,
+    loading: () => <LoadingScreen />,
+  },
+);
 
 export default function MapsPage() {
   return (
