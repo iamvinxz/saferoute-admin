c2253e8093a191a90fa13c2412074ba951cda189
iamvinxz <john.khervin13@gmail.com>
2026-05-18 13:14:46 +0800

    feat: added isLoading prop and create a skeleton ui for loading

--- components/dashboard/Card.tsx
+++ components/dashboard/Card.tsx
@@ -5,6 +5,7 @@ type Props = {
   icon: any;
   iconBg: string;
   iconColor: string;
+  isLoading: boolean;
 };
 
 const StatCard = ({
@@ -14,23 +15,41 @@ const StatCard = ({
   icon: Icon,
   iconBg,
   iconColor,
+  isLoading,
 }: Props) => {
   return (
     <div className="w-full bg-white rounded-md py-2 px-4 shadow-[0px_1px_2px_-px_rgba(0,0,0,0.25)] flex flex-col gap-5 lg:gap-9 lg:rounded-lg lg:px-5 lg:py-7 lg:shadow-[0px_1px_3.5px_-1px_rgba(0,0,0,0.25)]">
-      <div className="flex items-start justify-between">
-        <p className="text-[1.8rem] xl:text-4xl font-semibold text-[#303030]">
-          {count}
-        </p>
-        <div className={`${iconBg} p-1 rounded-xs lg:p-2 lg:rounded-md`}>
-          <Icon size={14} className={`${iconColor} lg:w-4.5! lg:h-4.5!`} />
-        </div>
-      </div>
-      <div>
-        <p className="text-[13px] font-medium text-[#303030] lg:text-lg">
-          {label}
-        </p>
-        <p className="md:block text-[9px] text-gray-400 lg:text-xs">{desc}</p>
-      </div>
+      {isLoading ? (
+        <>
+          <div className="flex items-start justify-between">
+            <div className="w-10 h-10 bg-slate-200 rounded-md animate-pulse-fast" />
+            <div className="w-7 h-6 bg-slate-200 rounded-md animate-pulse-fast" />
+          </div>
+          <div className="space-y-2">
+            <div className="w-17 h-3 bg-slate-200 rounded-md animate-pulse-fast" />
+            <div className="w-27 h-3 bg-slate-200 rounded-md animate-pulse-fast" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex items-start justify-between">
+            <p className="text-[1.8rem] xl:text-4xl font-semibold text-[#303030]">
+              {count}
+            </p>
+            <div className={`${iconBg} p-1 rounded-xs lg:p-2 lg:rounded-md`}>
+              <Icon size={14} className={`${iconColor} lg:w-4.5! lg:h-4.5!`} />
+            </div>
+          </div>
+          <div>
+            <p className="text-[13px] font-medium text-[#303030] lg:text-lg">
+              {label}
+            </p>
+            <p className="md:block text-[9px] text-gray-400 lg:text-xs">
+              {desc}
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
