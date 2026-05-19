a91bed210b4ef7caf0671270a732912d67c01cd4
John Khervin <john.khervin13@gmail.com>
2026-05-19 20:54:54 +0800

    feat: added useEffect to ensure every rehydrate user's data is fetched

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,29 @@
+"use client";
 import SideBar from "@/components/SideBar";
+import { useGetMeQuery } from "@/Redux/Services/authService";
+import { clearUser, setUser } from "@/state/slices/authSlice";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { data, isSuccess, isError } = useGetMeQuery();
+
+  console.log("data", data);
+
+  useEffect(() => {
+    if (isSuccess && data?.user) {
+      dispatch(setUser(data.user));
+    }
+
+    if (isError) {
+      dispatch(clearUser());
+      router.push("/auth/login");
+    }
+  }, [isSuccess, isError, data]);
+
   return (
     <div className="h-screen flex">
       <SideBar />
