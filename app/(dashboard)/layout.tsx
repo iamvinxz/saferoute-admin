06f28e8cbd6066f335f721aa85375545be4bfdef
John Khervin <john.khervin13@gmail.com>
2026-05-22 00:03:17 +0800

    feat: removed watcher to simplify layout file

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,25 +1,15 @@
 "use client";
 import SideBar from "@/components/SideBar";
 import { useGetMeQuery } from "@/Redux/Services/authService";
-import {
-  clearUser,
-  setUser,
-  setCoordinates,
-  setWatchId,
-} from "@/state/slices/authSlice";
+import { clearUser, setUser } from "@/state/slices/authSlice";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useGetAllSosAlertQuery } from "@/Redux/Services/sosService";
-import { RootState } from "@/state/store";
-import { isMobileDevice } from "@/lib/deviceHelper";
+import { useDispatch } from "react-redux";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
   const router = useRouter();
   const { data, isSuccess, isError } = useGetMeQuery();
-  const user = useSelector((state: RootState) => state.auth.user);
-  const { data: sosResponse } = useGetAllSosAlertQuery();
 
   // rehydrate user
   useEffect(() => {
@@ -31,32 +21,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       router.push("/auth/login");
     }
   }, [isSuccess, isError, data]);
-
-  // restart location tracking on refresh if rescuer has active rescue
-  useEffect(() => {
-    if (!isSuccess || !user || !sosResponse) return;
-
-    const hasActiveRescue = sosResponse?.alerts?.some(
-      (alert) =>
-        alert.status === "dispatched" && alert.rescuerId?._id === user?._id,
-    );
-
-    if (hasActiveRescue) {
-      let watchId: number;
-      watchId = navigator.geolocation.watchPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          dispatch(setCoordinates([latitude, longitude]));
-          dispatch(setWatchId(watchId));
-        },
-        (error) => console.error("Error getting location:", error.message),
-        {
-          enableHighAccuracy: isMobileDevice(),
-          maximumAge: 0,
-        },
-      );
-    }
-  }, [sosResponse, user, isSuccess]);
 
   return (
     <div className="h-screen flex">
