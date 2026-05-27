4b8555b9fefc1680db1b534001ea1f1f7744096e
John Khervin <john.khervin13@gmail.com>
2026-05-27 17:50:22 +0800

    feat: added middleware for by role routes

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,51 @@
+import { jwtDecode } from "jwt-decode";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const PROTECTED_ROUTES = ["/dashboard", "/maps", "/notification"];
 const AUTH_ROUTES = ["/auth/login"];
+const ADMIN_ONLY_ROUTES = ["/users"];
+const RESCUER_ONLY_ROUTES = ["/rescues"];
 
 export default function proxy(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-  // const token = true;
   const { pathname } = req.nextUrl;
 
   const isProtectedRoute = PROTECTED_ROUTES.some((route) =>
     pathname.startsWith(route),
   );
   const isAuthRoute = AUTH_ROUTES.some((route) => pathname.startsWith(route));
+  const isAdminOnly = ADMIN_ONLY_ROUTES.some((route) =>
+    pathname.startsWith(route),
+  );
+  const isRescuerOnly = RESCUER_ONLY_ROUTES.some((route) =>
+    pathname.startsWith(route),
+  );
 
   if (isProtectedRoute && !token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  // Already logged in → trying to access login page (back button case)
   if (isAuthRoute && token) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  // role-based protection
+  if (token && (isAdminOnly || isRescuerOnly)) {
+    try {
+      const decoded = jwtDecode<{ role: string }>(token);
+      const role = decoded.role;
+
+      if (isAdminOnly && role === "rescuer") {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      if (isRescuerOnly && role === "admin") {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+    } catch (error) {
+      console.error("Invalid token:", error);
+    }
   }
 
   const response = NextResponse.next();
