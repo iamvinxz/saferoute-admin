034ba522b09abb2189c16090867327031d935a0b
John Khervin <john.khervin13@gmail.com>
2026-06-18 12:27:44 +0800

    feat: added pagination params

--- Redux/Services/floodReportService.ts
+++ Redux/Services/floodReportService.ts
@@ -3,9 +3,13 @@ import { DELETE_SINGLE_FLOOD_REPORT, GET_ALL_FLOOD_REPORTS } from "./Endpoints";
 
 const floodReport = api.injectEndpoints({
   endpoints: (build) => ({
-    getAllFloodReport: build.query<FloodReportControllerGetAllResponse, void>({
-      query: () => ({
+    getAllFloodReport: build.query<
+      FloodReportControllerGetAllResponse,
+      FloodReportControllerGetAllRequest
+    >({
+      query: ({ page = 1, limit = 15 }) => ({
         url: GET_ALL_FLOOD_REPORTS,
+        params: { page, limit },
       }),
       providesTags: ["FloodReports"],
     }),
@@ -38,10 +42,25 @@ type FloodReportControllerGetAllResponse = {
     createdAt: string;
     updatedAt: string;
   }[];
+  pagination: FloodReportPagination;
+};
+
+type FloodReportControllerGetAllRequest = {
+  page: number;
+  limit: number;
 };
 
 type FloodReportControllerDeleteSingleRequest = {
   id: string;
+};
+
+type FloodReportPagination = {
+  totalReports: number;
+  totalPages: number;
+  currentPage: number;
+  limit: number;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
 };
 
 export const { useGetAllFloodReportQuery, useDeleteFloodReportMutation } =
