a2ab720a79298640bc14f0f88777db8ca6059f65
John Khervin <john.khervin13@gmail.com>
2026-06-19 13:48:46 +0800

    feat: added redux endpoint for get by status and get by depth

--- Redux/Services/floodReportService.ts
+++ Redux/Services/floodReportService.ts
@@ -1,5 +1,10 @@
 import { api } from "./APIService";
-import { DELETE_SINGLE_FLOOD_REPORT, GET_ALL_FLOOD_REPORTS } from "./Endpoints";
+import {
+  DELETE_SINGLE_FLOOD_REPORT,
+  GET_ALL_FLOOD_REPORT_BY_DEPTH,
+  GET_ALL_FLOOD_REPORT_BY_STATUS,
+  GET_ALL_FLOOD_REPORTS,
+} from "./Endpoints";
 
 const floodReport = api.injectEndpoints({
   endpoints: (build) => ({
@@ -10,6 +15,26 @@ const floodReport = api.injectEndpoints({
       query: ({ page = 1, limit = 15 }) => ({
         url: GET_ALL_FLOOD_REPORTS,
         params: { page, limit },
+      }),
+      providesTags: ["FloodReports"],
+    }),
+    getAllFloodReportByDepth: build.query<
+      FloodReportControllerGetAllResponse,
+      FloodReportControllerGetAllByDepth
+    >({
+      query: ({ depth, limit, page }) => ({
+        url: GET_ALL_FLOOD_REPORT_BY_DEPTH,
+        params: { depth, page, limit },
+      }),
+      providesTags: ["FloodReports"],
+    }),
+    getAllFloodReportByStatus: build.query<
+      FloodReportControllerGetAllResponse,
+      FloodReportControllerGetAllByStatus
+    >({
+      query: ({ status, page, limit }) => ({
+        url: GET_ALL_FLOOD_REPORT_BY_STATUS,
+        params: { status, page, limit },
       }),
       providesTags: ["FloodReports"],
     }),
@@ -27,6 +52,18 @@ const floodReport = api.injectEndpoints({
   }),
   overrideExisting: true,
 });
+
+type FloodReportControllerGetAllByDepth = {
+  depth: string;
+  page: number;
+  limit: number;
+};
+
+type FloodReportControllerGetAllByStatus = {
+  status: string;
+  page: number;
+  limit: number;
+};
 
 type FloodReportControllerGetAllResponse = {
   reports: {
@@ -63,5 +100,9 @@ type FloodReportPagination = {
   hasPrevPage: boolean;
 };
 
-export const { useGetAllFloodReportQuery, useDeleteFloodReportMutation } =
-  floodReport;
+export const {
+  useGetAllFloodReportQuery,
+  useDeleteFloodReportMutation,
+  useGetAllFloodReportByDepthQuery,
+  useGetAllFloodReportByStatusQuery,
+} = floodReport;
