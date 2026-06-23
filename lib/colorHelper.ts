2fe25ca59374dcdc3caedd63cd77c97271c5473a
John Khervin <john.khervin13@gmail.com>
2026-06-23 14:14:11 +0800

    feat: added color scheme for rejected and verified

--- lib/colorHelper.ts
+++ lib/colorHelper.ts
@@ -29,6 +29,14 @@ export const statusColors: Record<string, { badge: string; text: string }> = {
     badge: "bg-yellow-100",
     text: "text-yellow-700",
   },
+  Verified: {
+    badge: "bg-green-100",
+    text: "text-green-700",
+  },
+  Rejected: {
+    badge: "bg-red-100",
+    text: "text-red-700",
+  },
   Dispatched: {
     badge: "bg-blue-100",
     text: "text-blue-700",
